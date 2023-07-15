4600d61301a36da16857fa1970a1cabc65ea0f7e
KMS <fj7910@naver.com>
2023-07-15 12:53:39 +0900

    o

---
+++ section25/Ex25-1-sqlite-ddl.py
@@ -0,0 +1,35 @@
+'''
+Ex25-1-sqlite-ddl.py
+
+SQLite
+    오픈소스 관계형 데이터베이스
+    서버가 필요하지 않으며, 파일 기반 데이터베이스로 동작한다.
+    로컬 시스템에서 바로 사용할 수 있다.
+'''
+
+import sqlite3
+db_file = 'hr.db'
+
+conn = sqlite3.connect(db_file)
+cur = conn.cursor()
+
+cur.execute('''
+CREATE TABLE employees (
+    employee_id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number TEXT,
+    hire_data TEXT,
+    job_id TEXT,
+    salary REAL,
+    commission_pct REAL,
+    manager_id INTEGER,
+    department_id INTEGER
+)    
+''')
+
+
+
+
+
---
+++ section25/Ex25-2-sqlite-dml.py
@@ -0,0 +1,69 @@
+'''
+Ex25-2-sqlite-dml.py
+'''
+
+import sqlite3
+'''
+conn =sqlite3.connect('hr.db')
+cur = conn.cursor()
+
+#데이터 삽입
+sql = "INSERT INTO employees " \
+      "(first_name, last_name, email, " \
+      "hire_data, job_id, salary, department_id) " \
+      "VALUES (?, ?, ?, ?, ?, ?, ?)"
+cur.execute(sql, ('John', 'Doe', 'johndoe1@example.com',
+                  '2023-07-15', 'IT_PROG', 5000, 90))
+conn.commit()
+cur.close()
+conn.close()
+'''
+
+# 데이터 조회
+conn =sqlite3.connect('hr.db')
+cur = conn.cursor()
+
+sql = "SELECT * FROM employees WHERE employee_id = ?"
+cur.execute(sql, (1,))
+rows = cur.fetchall()
+print(rows)
+
+
+# 데이터 수정
+conn =sqlite3.connect('hr.db')
+cur = conn.cursor()
+sql = "UPDATE employees SET salary = ? WHERE employee_id = ?"
+cur.execute(sql, (5500, 1))
+conn.commit()
+
+cur.close()
+conn.close()
+
+# 데이터 조회
+conn =sqlite3.connect('hr.db')
+cur = conn.cursor()
+
+sql = "SELECT * FROM employees WHERE employee_id = ?"
+cur.execute(sql, (1,))
+rows = cur.fetchall()
+print(rows)
+
+# 데이터 삭제
+conn =sqlite3.connect('hr.db')
+cur = conn.cursor()
+sql = "DELETE FROM employees"
+cur.execute(sql)
+conn.commit()
+
+cur.close()
+conn.close()
+
+print('============= DELETE 후 ================')
+# 데이터 조회
+conn =sqlite3.connect('hr.db')
+cur = conn.cursor()
+
+sql = "SELECT * FROM employees "
+cur.execute(sql)
+rows = cur.fetchall()
+print(rows)
