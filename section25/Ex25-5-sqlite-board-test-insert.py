631484d0977a3b3f4d07f0d7605e63e08b867ad7
KMS <fj7910@naver.com>
2023-07-16 12:41:56 +0900

    o

---
+++ section25/Ex25-5-sqlite-board-test-insert.py
@@ -0,0 +1,29 @@
+'''
+Ex25-5-sqlite-board-test-insert.py
+'''
+
+import sqlite3
+
+conn = sqlite3.connect('py_board.db')
+cur = conn.cursor()
+
+# 데이터 삽입
+sql = "INSERT INTO PY_BOARD " \
+      "(BOARD_TITLE, BOARD_WRITER, BOARD_CONTENT) VALUES " \
+      "(?, ?, ?);"
+cur.execute(sql, ('test', 'dev', '테스트 중입니다.'))
+conn.commit()
+cur.close()
+conn.close()
+
+
+# # 데이터 조회
+conn = sqlite3.connect('py_board.db')
+cur = conn.cursor()
+
+sql = "SELECT * FROM PY_BOARD"
+cur.execute(sql)
+rows = cur.fetchall()
+print(rows)
+cur.close()
+conn.close()
